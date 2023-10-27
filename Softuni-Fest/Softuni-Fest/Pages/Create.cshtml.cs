e919d2e95fdf298c7ba2957f61c33c2e66c877a5
ChrisX21 <chrisalexiev@icloud.com>
2023-10-27 15:32:33 +0300

    added new product form and product display tablica

---
+++ Softuni-Fest/Softuni-Fest/Pages/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Softuni_Fest.Pages
+{
+    public class createModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
