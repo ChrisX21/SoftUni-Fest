f19448d3d1db66093de8e4038fe653292994532c
Jordan-Dimitrov <iordan.dimitrov.highschool@svetlina.net>
2023-10-28 11:07:47 +0300

    remove and update for products

--- Softuni-Fest/Softuni-Fest/Pages/Create.cshtml.cs
+++ Softuni-Fest/Softuni-Fest/Pages/Create.cshtml.cs
@@ -7,6 +7,7 @@ using Softuni_Fest.Repository;
 
 namespace Softuni_Fest.Pages
 {
+    [Authorize(Roles = "Business")]
     public class createModel : PageModel
     {
         private readonly IProductRepository _ProductRepository;
@@ -21,23 +22,37 @@ namespace Softuni_Fest.Pages
         }
         [BindProperty]
         public Product Product { get; set; }
-        [Authorize(Roles = "Business")]
         public async Task<IActionResult> OnPost()
         {
-            if (!User.Identity.IsAuthenticated)
-            {
-                return RedirectToPage("/Index");
-            }
-
             string userId = _UserManager.GetUserId(User);
 
             Product.VendorId = userId;
 
             if (!await _ProductRepository.AddProductAsync(Product))
             {
-                return StatusCode(400);
+                return Redirect("/Index");
             }
-            return StatusCode(200);
+            return Page();
+        }
+        //[BindProperty]
+        public string _ProductId { get; set; }
+        public async Task<IActionResult> OnDelete()
+        {
+            if(!await _ProductRepository.RemoveProductAsync(Product))
+            {
+                return Redirect("/Index");
+            }
+            return Page();
+        }
+        public async Task<IActionResult> OnUpdate()
+        {
+            Product product = await _ProductRepository.GetProductAsync(_ProductId);
+            product = Product;
+            if (!await _ProductRepository.UpdateProductAsync(product))
+            {
+                return Redirect("/Index");
+            }
+            return Page();
         }
     }
 }
