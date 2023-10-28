ecff212f223bc08fc09081d4147a6e37d73f4604
ChrisX21 <chrisalexiev@icloud.com>
2023-10-29 00:59:20 +0300

    added a whole bunch of new features and bugs

---
+++ Softuni-Fest/Softuni-Fest/Pages/Product.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Softuni_Fest.Pages
+{
+    public class ProductModel : PageModel
+    {
+
+        public void OnGet()
+        {
+        }
+    }
+}
